070632210a4f4bd7a7cb1559bce0e160506f59fe
Imaad Uddin <imaaduddin@icloud.com>
2020-12-24 18:35:38 -0600

    added styles

--- src/components/menu-item/directory/directory.component.jsx
+++ src/components/menu-item/directory/directory.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import MenuItem from "../menu-item.component";
 
+import "./directory.styles.scss";
+
 class Directory extends React.Component {
   constructor() {
     super();
