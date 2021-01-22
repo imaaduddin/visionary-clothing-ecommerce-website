73850559911a04fa6cf58d1b77064e772488467c
Imaad Uddin <imaaduddin@icloud.com>
2021-01-21 20:18:16 -0600

    resolved a bug

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,19 +6,21 @@ import { ReactComponent as Logo } from "../../assests/crown.svg";
 import "./header.styles.scss";
 
 const Header = () => {
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
       </Link>
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default Header;
