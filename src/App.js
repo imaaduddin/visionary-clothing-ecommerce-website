5b17c73441594bb12cd9efda9065c775a79eef54
Imaad Uddin <imaaduddin@icloud.com>
2021-01-28 20:02:10 -0600

    created new component for sign in and sign up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component.jsx";
 import Header from "./components/header/header.component.jsx";
+
 
 function App() {
   return (
